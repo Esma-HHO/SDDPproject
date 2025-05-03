3440649e56b98e538084c4829cd7f1aa14fcd2d9
hacer00 <149515035+hacer00@users.noreply.github.com>
2025-05-03 12:34:25 +0300

    Update views.py

--- backend/sahibesinden/user/views.py
+++ backend/sahibesinden/user/views.py
@@ -1,2 +1,28 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+
+#General User
+def signUp(self):
+    pass
+def logIn(self):
+    pass
+def searchAndFilter(self):
+    pass
+def showAd(self):
+    pass
+#Member
+def addingAd(self):
+    pass
+def updateAd(self):
+    pass
+def payment(self):
+    pass
+def message(self):
+    pass
+def editProfile(self):
+    pass
+#Admin
+def deleteMember(self):
+    pass
+def deleteAd(self):
+    pass
